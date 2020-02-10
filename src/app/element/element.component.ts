9eb5bb921baa8364ae8abd4a2c347229cec1da14
TamNhiDuong <yanxinyee@gmail.com>
2020-02-10 23:12:53 +0200

    how to pass data from parent comp to child

--- src/app/element/element.component.ts
+++ src/app/element/element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-element',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./element.component.css']
 })
 export class ElementComponent implements OnInit {
+  @Input() element: {type: string, name: string, content: string};
 
   constructor() { }
 
