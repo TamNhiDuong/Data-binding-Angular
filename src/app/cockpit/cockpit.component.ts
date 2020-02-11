52d4e1d35eeff96686adb4e0f8f8925cab359405
TamNhiDuong <yanxinyee@gmail.com>
2020-02-11 14:47:30 +0200

    binding event from child to parent components

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  serverElements = [];
+  @Output() createServer= new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() createBluePrint= new EventEmitter<{serverName: string, serverContent: string}>();;
   newServerName = '';
   newServerContent = '';
 
@@ -14,21 +15,23 @@ export class CockpitComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+ 
   onAddServer() {
-    //this.serverElements.push({
-     // type: 'server',
-     // name: this.newServerName,
-     // content: this.newServerContent
-    //});
+    this.createServer.emit(
+      {
+        serverName: this.newServerName, 
+        serverContent: this.newServerContent
+      }
+    )
   }
 
   onAddBlueprint() {
-    //this.serverElements.push({
-    //  type: 'blueprint',
-    //  name: this.newServerName,
-    //  content: this.newServerContent
-    //});
+    this.createBluePrint.emit(
+      {
+        serverName: this.newServerName, 
+        serverContent: this.newServerContent
+      }
+    )
   }
 
 }
